72876ead0b96ce017e7a12a1b625c9318e4ec14c
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-05-10 21:39:27 -0600

    basis of Requests.Py created

--- Brambl-Py/Requests.py
+++ Brambl-Py/Requests.py
@@ -1,8 +1,18 @@
-import asyncio
 
-async def BramblRequest():
-    pass    
+print('everything is working')
+url = "https://valhalla.torus.topl.co/" #temp name, make sure to change and clean
 
-async def chainInfo():
-    pass
 
+async def BramblRequest(routeInfo, params, obj): #obj is meant for the self of request
+    print('this is a placeholder')
+    route = routeInfo.route
+
+
+class Requests():
+    def __init__(self,url,apiKey):
+        self.url = url
+        self.apiKey = apikey
+
+    async def chainInfo():
+        params = {}
+        return BramblRequest()
