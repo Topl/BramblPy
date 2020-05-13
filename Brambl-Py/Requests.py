b7eec236913a594812db08746400e58873873f66
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-05-13 09:58:11 -0600

    chainInfo created, sending json function created

--- Brambl-Py/Requests.py
+++ Brambl-Py/Requests.py
@@ -2,43 +2,65 @@
 import json
 import requests
 import asyncio
-
+import os
+import sys
+import settings
 print('everything is working')
 url = "https://valhalla.torus.topl.co/" #temp name, make sure to change and clean
 
 def testfunc():
     print('i am here')
 
-
-async def BramblRequest(routeInfo, params, obj): #obj is meant for the self of request
-    print('this is a placeholder')
-    route = routeInfo.route#edit
-    
-
-
-
-class Requests():
-
-    
-
-    def __init__(self,url,apiKey):
-        self.url = url
-        self.apiKey = apiKey
-        
-    payload = {
+'''routeInfo = [route, method,ID]'''
+def BramblRequest(method,routeInfo, params, obj): #obj is meant for the self of request,rename method
+    body = {
             "jsonrpc": "2.0",
-            "id": "30",
+            "id": "1",
             "method": "info",
             "params": [{}]
         }
-    payload = json.dumps(payload)
+    response = requests.request(str(method).upper(),obj.url+routeInfo[0], json= body, allow_redirects = True,headers = obj.headers)
+    return response
+
+
+class Requests():
+    def __init__(self,url,apiKey):
+        self.url = url
+        self.apiKey = apiKey
+        self.headers = {
+            "Content-Type": "application/json",
+             'x-api-key': self.apiKey
+        }
+
+        
 
     def chainInfo(self):
+        params = {}
         route = 'debug/'
-        response = requests.request('POST',self.url+route, allow_redirects = False, timeout = 5)#temp value
-        return response#not presenting correctly
+        method = 'info'
+        Id = "1"
+        connectMethod = 'POST'
+        return BramblRequest(connectMethod,[route,method,Id],params,self)
+    
+    def sendParam(self,params):
+        params = json.dumps(params)
+        route = 'debug/'
+        method = 'info'
+        Id = "1"
+        connectMethod = 'POST'
+        return BramblRequest(connectMethod,[route,method,Id],params,self)
 
-    def paramReq(self):
-        route = 'debug/'
-        response = requests.request('POST',self.url+route, data = payload, allow_redirects = False, timeout = 5)#temp value
-        return response#not presenting correctly 
+
+
+b = Requests(url,os.getenv('VALHALLA_KEY'))
+
+x = b.chainInfo()
+print(x.text)
+
+
+y = b.sendParam({'Test':'qwert','test2':'ewrwe'})
+print(y.text)
+
+
+
+
