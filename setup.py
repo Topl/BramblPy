a6d0bb0a7de68ba1358f2047f78a4cc099c02db2
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-05-19 15:25:50 -0600

    setup.py updated,requirements.txt added

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
-from distutils.core import setup
-setup(
-  name = 'LokiPy',         # How you named your package folder (MyLib)
-  packages = ['LokiPy'],   # Chose the same as "name"
-  version = '0.11',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'First working version of LokiPy - a Python API wrapper for interacting with the Topl blockchain',   # Give a short description about your library
-  author = 'Yamir Tainwala',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/Topl/LokiPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Topl/LokiPy/archive/0.11.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'base58',
-          'requests',
-          'pyblake2'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+import setuptools
+
+with open('README.md','r') as fh:
+  longDescription = fh.read()
+
+setuptools.setup(
+  name = 'Brambl-Py',
+  version = '0.0.1',
+  author = '',
+  author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
