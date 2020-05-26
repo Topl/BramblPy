c96f836dc7026d065db79297f1d9b33def2482f5
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-05-26 11:23:48 -0600

    blake2b test updated

--- brambl/utils.py
+++ brambl/utils.py
@@ -1,15 +1,6 @@
-import hashlib
-#from Crypto.Hash import blake2b
-
-
-m = hashlib.blake2b()
-m.update(b'Hello World')
-print(m.digest())
-print(m.hexdigest())
-
-'''
-#for crypto.hash, does not work#
-obj = blake2b.new(digest_bits=512)
+from Crypto.Hash import BLAKE2b
+import curve25519
+obj = BLAKE2b.new(digest_bits=512)
 obj.update(b'Hello World')
-print(obj.hexidigest())
-'''
+print(obj.digest())
+
