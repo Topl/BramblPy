081aff001ef093c39caae73700c9f7781137ffcb
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-06-17 16:34:02 -0600

    getMAC built

--- brambl/modules/KeyManager.py
+++ brambl/modules/KeyManager.py
@@ -1,8 +1,11 @@
+import os
 from Crypto.Hash import BLAKE2b
 from Crypto.Hash import keccak
+from Crypto.Protocol.KDF import scrypt
 import axolotl_curve25519 as curve
 import base58
 import json
+from binascii import hexlify
 
 
 defaultOptions = {
@@ -20,6 +23,24 @@ defaultOptions = {
 
 def isFunction(f):
     return callable(f)
+
+def str2pybuf(string,enc):
+    if type(string) == 'str':
+        string2 = bytes(string)
+
+    if enc == 'base58':
+        return base58.b58decode(string)
+
+
+def getMAC(derivedKey,ciphertext):
+    keccak256 = keccak.new(digest_bits=256)
+    keccak256.update(derivedKey[16:]+str2pybuf(ciphertext,'base58'))
+    return keccak256.digest()
+
+key = scrypt('password','salt',32,N=2**14,r=8,p=1)
+
+MUC = getMAC(key,'ststsadf')
+print(hexlify(MUC))
 
 class KeyManager():
 
@@ -42,10 +63,17 @@ class KeyManager():
         except:
             self.constants = defaultOptions
 
- 
+    def verify(self,publicKey,message,signature,cb):
+        
 
+        if  not isFunction(cb):
+            return curve.verifySignature(publicKey,message,signature)#returns 0 if verified
+        cb(curve.verifySignature(publicKey,message,signature))
+        
+    
 
 new = KeyManager({'password': 'pasz'})
+
 
 
 
