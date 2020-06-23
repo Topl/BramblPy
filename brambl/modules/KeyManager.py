965072ebbf0c84e37dcdb517dc63e3ce9ba46a67
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-06-23 00:41:20 -0600

    keyManager functions built out, some of keymanager class built

--- brambl/modules/KeyManager.py
+++ brambl/modules/KeyManager.py
@@ -8,6 +8,7 @@ import base58
 import json
 from binascii import hexlify
 import pyaes
+import jks
 
 defaultOptions = {
     'cipher': 'aes-256-ctr',
@@ -25,7 +26,7 @@ defaultOptions = {
 def isFunction(f):
     return callable(f)
 
-def str2pybuf(string,enc):
+def str2pybuf(string,enc='base58'):
     if type(string) == 'str':
         string2 = bytes(string)
 
@@ -35,9 +36,15 @@ def str2pybuf(string,enc):
 
 def encrypt(plaintext,key,iv,algo):
     if algo == 'aes-256-ctr':
-        cipher = pyaes.AESModeOfOperationCTR
-        #cipher = pyaes.AESModeOfOperationCTR
-
+        key = bytearray(key)
+        aes = pyaes.AESModeOfOperationCTR(key,pyaes.Counter(iv))
+        ciphertext = aes.encrypt(plaintext)
+        return ciphertext
+        
+def decrypt(ciphertext,key,iv,algo):
+    if algo == 'aes-256-ctr':
+        aes = pyaes.AESModeOfOperationCTR(key,pyaes.Counter(iv))
+        return aes.decrypt(ciphertext).decode('utf-8')
 
 def getMAC(derivedKey,ciphertext):
     keccak256 = keccak.new(digest_bits=256)
@@ -84,12 +91,56 @@ def deriveKey(password,salt,kdfParams,cb):
     cb(scrypt(password,salt,dkLen,N,r,p,num_keys=1))
 
 def marshal(derivedKey,keyObject,salt,iv,algo):
-    pass
+    if algo == 'aes-256-ctr':
+        ciphertext = encrypt(keyObject['privateKey'],derivedKey,iv)
+        keyStorage = {
+            'publicKeyId': base58.b58encode(keyObject['privateKey']),
+            'crypto': {
+                'cipher': algo,
+                'ciphertext': base58.b58encode(ciphertext),
+                'cipherParams': {'iv': base58.b58encode(iv)},
+                'mac': base58.b58encode(getMAC(derivedKey,ciphertext)),
+                'kdf': 'scrypt',
+                'kdfSalt': base58.b58encode(salt)
+            }
+        }
+    return keyStorage
+
+def dump(password,keyObject,options,cb):
+    kdfParams = option['kdfParams'] or options['scrypt']
+    iv = str2pybuf(keyObject['iv'])
+    salt = str2pybuf(keyObject['salt'])
+    privateKey = str2pybuf(keyObject['privateKey'])
+    publicKey = str2pybuf(keyObject['publicKey'])
+    if not isFunction(cb):
+        return marshal(deriveKey(password,salt,kdfParams),{'privateKey': privateKey,'publicKey':publicKey},salt,iv,options['cipher'])
     
+    return cb(marshal(deriveKey(password,salt,kdfParams),{'privateKey': privateKey,'publicKey':publicKey},salt,iv,options['cipher']))
 
 
+def recover(password,keyStorage,kdfParams,cb):
+    
+    def verifyAndDecrypt(derivedKey,iv,ciphertext,mac,algo):
+        if getMAC(derivedKey,ciphertext) != mac:
+            raise Exception("message authentication code mismatch")
+        return decrypt(ciphertext,derivedKey,iv,algo)
 
+    iv = str2pybuf(keyStorage['crypto']['cipherParams']['iv'])
+    salt = str2pybuf(keyStorage['crypto']['kdfSalt'])
+    ciphertext = str2pybuf(keyStorage['crypto']['cipherText'])
+    mac = str2pybuf(keyStorage['crypto']['mac'])
+    algo = str2pybuf(keyStorage['crypto']['cipher'])
 
+    if not isFunction(cb):
+        return verifyAndDecrypt(deriveKey(password,salt,kdfParams),iv, ciphertext, mac, algo)
+
+    return cb(verifyAndDecrypt(deriveKey(password,salt,kdfParams),iv, ciphertext, mac, algo))
+
+def generateKeystoreFilename(publicKey):
+    pass
+    #TODO: I am unsure how to  implement the python javascripy key module
+    
+ 
 class KeyManager():
 
     def __init__(self,params):
@@ -104,7 +155,9 @@ class KeyManager():
             self.isLocked = False
             self.password = password
             self.keyStorage = keyStorage
+            #add sk
             #add recover if statement
+
 
         try:
             self.constants = params['constants']
@@ -112,17 +165,17 @@ class KeyManager():
             self.constants = defaultOptions
 
     def verify(self,publicKey,message,signature,cb):
-        
-
         if  not isFunction(cb):
             return curve.verifySignature(publicKey,message,signature)#returns 0 if verified
         cb(curve.verifySignature(publicKey,message,signature))
-        
+
+    def lockKey():
+        self.isLocked = True 
     
-
-new = KeyManager({'password': 'pasz'})
-
-print(new.constants)
+    def sign(message):
+        if self.isLocked:
+            raise Exception('The key is currently locked. Please unlock and try again.')
+        return curve.calculateSignature(os.urandom(64),sk,message)
 
 
 
