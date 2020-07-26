93c37a52226d252e0b9cc73a25ed71c9fa0d51c3
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-07-26 15:38:04 -0600

    some changes

--- tests/testScripts/integrationTestRequest.py
+++ tests/testScripts/integrationTestRequest.py
@@ -13,8 +13,9 @@ class TestIntegrationRequests(unittest.TestCase):
         self.brambl = Requests.Requests()#implement check if local node is running
 
     def testChainInfo(self):
-        print()
         self.assertEqual(json.loads(self.brambl.chainInfo())['jsonrpc'],'2.0')
+
+    
 
 if __name__ == '__main__':
     unittest.main()
