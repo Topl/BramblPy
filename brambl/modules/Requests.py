3b1fe4b5d17d6c4ad386f234bcc83a9594334d2e
Kenneth Sterling Wells <72569701+sterlingwellscaffeine@users.noreply.github.com>
2021-10-12 16:56:45 -0500

    Made a small transactionId validator to test unit testing constructs (#107)
    * doc: Added documentation around Contributing and Code of Conduct (#106)
    
    * doc: Added documentation around Contributing and Code of Conduct
    
    * Small fix
    
    Co-authored-by: Sterling Wells <s.wells@topl.me>
    
    * Made a small transactionId validator to test unit testing constructs

--- brambl/modules/Requests.py
+++ brambl/modules/Requests.py
@@ -9,6 +9,7 @@ Documentation for Brambl-layer is available at https://Requests.docs.topl.co
 """
 # Dependencies
 import json
+from base58 import b58decode
 import requests
 import asyncio
 import os
@@ -845,3 +846,20 @@ class Requests():
         route = 'debug/'
         method = 'generators'
         return BramblRequest(self,{'route':route,'method': method,'id':ID},params).text
+
+
+def validate_txId(txid):
+    if (not isinstance(txid, str)):
+        raise TypeError("TransactionId must be a str object")
+    try: 
+        decodedTxId = b58decode(txid)
+        if (decodedTxId.__len__ != 33):
+            raise ValueError(
+                "TransactionId must be 33 bytes long after being decoded fromBase58, not" "'{}'".format(decodedTxId.__len__)
+            )
+    except ValueError as e:
+        raise ValueError(
+            "Transaction ID must be a Base58 encoded string, not "
+            "'{}'".format(txid)
+        )
+    return txid
