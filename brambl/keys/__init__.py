b2a9c9c072d0ea5341200940c0bf99d8796000c3
Sterling Wells <s.wells@topl.me>
2021-10-27 11:43:18 -0500

    Rephrased

--- brambl/keys/__init__.py
+++ brambl/keys/__init__.py
@@ -7,7 +7,7 @@ import warnings
 if sys.version_info.major < 3:
     warnings.simplefilter('always', DeprecationWarning)
     warnings.warn(DeprecationWarning(
-        "The `eth-keys` library is dropping support for Python 2.  Upgrade to Python 3."
+        "The `brambl.keys` library is dropping support for Python 2.  Upgrade to Python 3."
     ))
     warnings.resetwarnings()
 
