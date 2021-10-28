47eddf646f6524553e5684fb20abda20ab8364f7
Sterling Wells <s.wells@topl.me>
2021-10-28 15:48:56 -0500

    Updated signature around keyfile initialization

--- brambl/keyfile/__init__.py
+++ brambl/keyfile/__init__.py
@@ -7,6 +7,6 @@ import sys
 if sys.version_info.major < 3:
     warnings.simplefilter('always', DeprecationWarning)
     warnings.warn(DeprecationWarning(
-        "The `eth-keyfile` library is dropping support for Python 2.  Upgrade to Python 3."
+        "The `topl-keyfile` library is dropping support for Python 2.  Upgrade to Python 3."
     ))
     warnings.resetwarnings()
